127caf7728186be3716b627b68b45a709f2f767a
Garbin <garbin@ad.garmin.com>
2018-01-24 14:36:46 +0100

    Components created

--- todoreact/src/App.js
+++ todoreact/src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Date from './Date.js';
+import TaskList from './TaskList.js';
+import Avatar from './Avatar.js';
+import AddButton from './AddButton.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={{padding: '30px 30px'}}>
+        <Avatar />
+        <br />
+        <Date />
+        <br />
+        <TaskList />
+        <br />
+        <AddButton />
       </div>
     );
   }
